a8981d2b5ae5aec432948c200923095c6375664c
Natnael Mekonnen <nhattymack204@gmail.com>
2023-10-25 13:57:29 +0300

    feature(staff): added the main router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,10 +4,12 @@ import requestRouter from "./request.routes";
 import orderRouter from "./order.routes";
 import paymentRouter from "./payment.routes";
 import statisticsRouter from "./statistics.routes";
+import staffRouter from "./staff.routes";
 
 const router = Router();
 
 router.use("/account", accountRouter);
+router.use("/staff", staffRouter);
 router.use("/request", requestRouter);
 router.use("/order", orderRouter);
 router.use("/payment", paymentRouter);
